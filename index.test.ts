964e02d74a39f8e1c7ca8ed5182e805677990ad1
Ramtin <Ramtin.Abadi@gmail.com>
2021-06-19 09:31:46 +0530

    Added test for 24 hours regexes

--- index.test.ts
+++ index.test.ts
@@ -37,6 +37,51 @@ describe("Regex Tests", () => {
         expect(minutesWithLeadingZero.test("111")).toBeTruthy();
         expect(minutesWithLeadingZero.test("0")).toBeFalsy();
 
+        
+        // The 24 hours time regexes (yes, the plural of regex is regexes) enforce the start and the end of the string. So, a case in the middle of the string will not be matched
+        expect(time24Hours.test("0")).toBeFalsy();
+        expect(time24Hours.test("00")).toBeFalsy();
+        expect(time24Hours.test("23")).toBeFalsy();
+        expect(time24Hours.test("23:")).toBeFalsy();
+        expect(time24Hours.test("23:1")).toBeFalsy();
+        expect(time24Hours.test("23:s1")).toBeFalsy();
+        expect(time24Hours.test("23:60")).toBeFalsy();
+        expect(time24Hours.test("24:11")).toBeFalsy();
+        expect(time24Hours.test("24s11")).toBeFalsy();
+        expect(time24Hours.test("2s:11")).toBeFalsy();
+        expect(time24Hours.test("s22:11")).toBeFalsy();
+        expect(time24Hours.test("22:11s")).toBeFalsy();
+        expect(time24Hours.test("00:11")).toBeTruthy();
+        expect(time24Hours.test("23:59")).toBeTruthy();
+        expect(time24Hours.test("18:42")).toBeTruthy();
+        expect(time24Hours.test("04:05")).toBeTruthy();
+
+
+        expect(time24HoursWithSeconds.test("0")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("00")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("23")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("23:")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("23:1")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("23:s1")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("23:60")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("24:11")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("24s11")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("2s:11")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("s22:11")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("22:11s")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("00:11")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("23:59")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("18:42")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("04:05")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("00:11s11")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("23:59:1s")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("18:42:s1")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("04:05:60")).toBeFalsy();
+        expect(time24HoursWithSeconds.test("00:11:20")).toBeTruthy();
+        expect(time24HoursWithSeconds.test("23:59:59")).toBeTruthy();
+        expect(time24HoursWithSeconds.test("18:42:00")).toBeTruthy();
+        expect(time24HoursWithSeconds.test("04:05:06")).toBeTruthy();
+
     })
 
 });
