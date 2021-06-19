227f0af682f0d94d382dc90c6f3a91c7329d4498
Ramtin <Ramtin.Abadi@gmail.com>
2021-06-19 08:55:57 +0530

    Improved the time regex

--- index.ts
+++ index.ts
@@ -1,15 +1,15 @@
 /** Used in cases such as time expressions like 14:23 */
-let hoursWithLeadingZero = /0{0}[0-23]{2}/;
+export let hoursWithLeadingZero = /0{0}[0-23]{2}/;
 
 /** Used in cases such as time expressions like 14:23 */
-let minutesWithLeadingZero = /0{0}[0-59]{2}/;
+export let minutesWithLeadingZero = /0{0}[0-59]{2}/;
 
 
 /** Regex matching 14:23 */
-export let time24Hours = new RegExp(hoursWithLeadingZero.source + "\:" + minutesWithLeadingZero.source);
+export let time24Hours = new RegExp("^" + hoursWithLeadingZero.source + "\:" + minutesWithLeadingZero.source + "$");
 
 /** Regex matching 14:23:56 */
-export let time24HoursWithSeconds = new RegExp(hoursWithLeadingZero.source + "\:" + minutesWithLeadingZero.source + "\:" + minutesWithLeadingZero.source);
+export let time24HoursWithSeconds = new RegExp("^" + hoursWithLeadingZero.source + "\:" + minutesWithLeadingZero.source + "\:" + minutesWithLeadingZero.source + "$");
 
 
 /** Regex matching example@example.com */
